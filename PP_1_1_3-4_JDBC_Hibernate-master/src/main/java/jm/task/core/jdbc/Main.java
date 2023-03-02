5e53abe5db4e5280dfd91c4438b3637b66c60398
Aleksander24007 <120310480+Aleksander24007@users.noreply.github.com>
2023-03-02 14:06:08 +0300

    Add files via upload

---
+++ PP_1_1_3-4_JDBC_Hibernate-master/src/main/java/jm/task/core/jdbc/Main.java
@@ -0,0 +1,28 @@
+package jm.task.core.jdbc;
+
+
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+
+public class Main {
+    public static void main(String[] args) {
+        // реализуйте алгоритм здесь
+
+        UserService userService = new UserServiceImpl();
+
+        userService.createUsersTable();
+
+        userService.saveUser("Name1","Last1",(byte) 20);
+        userService.saveUser("Name2","Last2",(byte) 23);
+        userService.saveUser("Name3","Last3",(byte) 32);
+        userService.saveUser("Name4","Last4",(byte) 43);
+
+        userService.removeUserById(1);
+        userService.getAllUsers();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
+
+    }
+}
---
+++ PP_1_1_3-4_JDBC_Hibernate-master/src/main/java/jm/task/core/jdbc/util/Util.java
@@ -0,0 +1,26 @@
+package jm.task.core.jdbc.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Util {
+    // реализуйте настройку соеденения с БД
+
+    public Util() {
+    }
+    private static final String nameUser = "root";
+    private static final String password = "root";
+    private static final String url = "jdbc:mysql://localhost:3306/mysql";
+    private  static Connection connection;
+
+    static {
+        try {
+            connection = DriverManager.getConnection(url,nameUser,password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
+}
